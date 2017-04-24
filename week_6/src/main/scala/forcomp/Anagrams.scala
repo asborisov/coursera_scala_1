170ab1291ab46b82ef7e32a569b5b9db8c0c1ceb
Artem Borisov <timan.b@gmail.com>
2017-04-24 17:37:04 +0700

    Remove unnecessary pattern matching

--- week_6/src/main/scala/forcomp/Anagrams.scala
+++ week_6/src/main/scala/forcomp/Anagrams.scala
@@ -151,10 +151,9 @@ object Anagrams {
   def sentenceAnagrams(sentence: Sentence): List[Sentence] = {
     def loop(occurrences: Occurrences): List[Sentence] = {
       if (occurrences.isEmpty) List(List())
-      else combinations(occurrences).flatMap(wordAnagrams) match {
-        case words => words.flatMap(word => loop(subtract(occurrences, wordOccurrences(word))).map(word :: _))
-        case _ => List()
-      }
+      else combinations(occurrences)
+        .flatMap(wordAnagrams)
+        .flatMap(word => loop(subtract(occurrences, wordOccurrences(word))).map(word :: _))
     }
 
     val sOccurrences = sentenceOccurrences(sentence)
